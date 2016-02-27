bfa68f9708c181d428ae3557d13b2b98ea2202a7
christinmb <acristina.ahumada@gmail.com>
2016-02-27 14:17:29 -0600

    Mi app Django Girls, primer commit

---
+++ prueba1.py
@@ -0,0 +1,25 @@
+#print('Hola Mundo')
+#diccionario={'nombre':'Cristina','mes':'Marzo','numero_favorito':'10'}
+#for x in diccionario:
+#    print(diccionario[x])
+#    a=len(diccionario[x])
+#    if a >= 5:
+#        print(x)
+#        print(diccionario[x])
+#    elif a<=5:
+#        print(x)
+#        print(diccionario[x])
+#    else:
+#        print('Este valor no tiene más de 5 letras')
+#    print('************')
+
+num1=input('Valor 1: ')
+num2=input('Valor 2: ')
+#def mul(num1,num2):
+x=int(num1)
+y=int(num2)
+print("El resultado de la multiplicación es: ")
+print(x*y)
+
+
+#mul()
